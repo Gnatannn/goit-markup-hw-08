c57f45f07d8ee96c71a86e31d5296618589b4c7c
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-05-22 23:29:42 +0300

    5

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,7 +1,7 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector(".menu-open-btn"),
-        closeMenuBtn: document.querySelector(".menu-close-btn"),
+        openMenuBtn: document.querySelector(".menu-open-button"),
+        closeMenuBtn: document.querySelector(".menu-close-button"),
         menu: document.querySelector(".mob-menu"),
         body: document.querySelector(".body"),
     };
